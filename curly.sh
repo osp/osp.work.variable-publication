2a02b3044489476cd636e9965ee0edcd6637859e
Alexandre Leray <alexandre@stdin.fr>
2014-05-29 15:27:00 +0200

    Fix a wrong link (styles.css -> styles.less)

--- curly.sh
+++ curly.sh
@@ -2,7 +2,7 @@
 while true;
 do
  curl http://osp.kitchen:9999/p/variable_publication/export/txt > content.html
- curl http://osp.kitchen:9999/p/variable_publication.css/export/txt > styles.css
+ curl http://osp.kitchen:9999/p/variable_publication.css/export/txt > styles.less
  sleep 15
 done
 
