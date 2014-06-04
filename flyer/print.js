5484d3a3535aa864dddf15ba9aca9e5685b6b518
gijs <gijs@de-heij.com>
2014-06-05 00:34:19 +0200

    First design for the flyer

---
+++ flyer/print.js
@@ -0,0 +1,72 @@
+// Choose the number of pages of the document
+var nb_page = 2;
+
+
+// Loads main content into <section id="container">
+$("section#container").load("content.html");
+
+$(window).load(function(){
+    // __________________________________ DEBUG __________________________________ //
+    $("button#debug").click(function(e){
+        e.preventDefault();
+        $(this).toggleClass("button-active");
+        $("html").toggleClass("debug");
+    });
+
+
+    // __________________________________ HIGH RESOLUTION __________________________________ //
+    $("button#hi-res").click(function(e){
+        e.preventDefault();
+        $(this).toggleClass("button-active");
+        $("html").toggleClass("export");
+        $("img").each(function(){
+            var hires = $(this).attr("data-alt-src");
+            var lores = $(this).attr("src");
+            $(this).attr("data-alt-src", lores)
+            $(this).attr("src", hires)
+        });
+    });
+
+
+    // __________________________________ PRINT MARKS __________________________________ //
+    var doc_height = $("body").height();
+    var page_height = $("#master-page").height(); 
+
+    for (i = 1; i <= nb_page; i++){
+        $("#master-page").clone().addClass("preview-page").attr("id","page-"+i).insertBefore($("#master-page"));
+    }
+    $("#master-page").hide();
+
+
+    // __________________________________ TOC __________________________________ //
+    $(".preview-page").each(function(){
+        page = $(this).attr("id");
+        $("#toc-pages").append("<li><a href='#" + page + "'>" + page.replace("-", " ") + "</a></li>")
+    });
+    $("#goto").click(function(e){
+        e.preventDefault();
+        $(this).toggleClass("button-active");
+        $("#toc-pages").toggle();
+    });
+
+
+    // __________________________________ MOVEABLE ELEMENTS __________________________________ //
+    $("div.moveable").
+        append("<div class='properties button'>Properties</div>").
+        draggable({
+                cursor: "move",
+                stack: "div.moveable", 
+        }).
+        resizable();
+
+    $('.properties').on('click', function() {
+        var top = $(this).parent().css('top');
+        var left = $(this).parent().css('left');
+        var width = $(this).parent().width();
+        var height = $(this).parent().height();
+        var p = new Popelt({
+            title: "Properties to copy/paste into this object's style attribute.",
+            content: 'style="top: ' + top + '; left: ' + left + '; width: ' + width + 'px; height: ' + height + 'px;"',
+        }).show();
+    });
+});
